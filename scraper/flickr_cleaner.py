aa5a9fe27058b614c657502e8e2c4ff780bf5abb
Matteo Foglio <matteo.foglio@outlook.com>
2018-10-05 19:09:01 -0500

    Add scraper skeleton

---
+++ scraper/flickr_cleaner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Matteo Foglio
+"""
+
+def read_flickr_IDs(json_filepath):
+    pass
+
+def clean_flickr_IDs(list_flickr_ID):
+    pass
+
+def download_images(list_flickr_ID):
+    pass
---
+++ scraper/flickr_preprocessing_database.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Matteo Foglio
+"""
+
+class FlickrPreprocessingDB():
+
+    def __init__(self):
+        # load database
+        pass
+
+    def __load_db(self):
+        pass
+
+    def __save_db(self):
+        pass
+
+    def get_status(self, flickr_ID):
+        pass
+
+    def get_flickr_ID(self, wildbook_ID):
+        pass
+
+    def is_new(self, flickr_ID):
+        pass
+
+    def set_status(self, flickr_ID, status):
+        pass
+
+    def download_image(self, flickr_ID):
+        pass
+
+    def delete_image(self, flickr_ID):
+        pass
